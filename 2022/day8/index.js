3513f354fa2944ce1a6231ca21d3bad2f0b24cdb
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-20 15:11:11 +0100

    - finishing scenic score

--- 2022/day8/index.js
+++ 2022/day8/index.js
@@ -17,42 +17,6 @@ function generateTreeMap() {
   });
 }
 
-// function puzzleOne() {
-//   let visibleTrees = 0;
-
-//   treeMap.forEach((treeRow, y) => {
-//     treeRow.forEach((tree, x) => {
-//       const ABOVE = treeMap[y - 1] != null ? treeMap[y - 1][x] : null;
-//       const RIGHT = treeRow[x + 1] != null ? treeRow[x + 1] : null;
-//       const BELOW = treeMap[y + 1] != null ? treeMap[y + 1][x] : null;
-//       const LEFT = treeRow[x - 1] != null ? treeRow[x - 1] : null;
-//       const surroundingTrees = [ABOVE, RIGHT, BELOW, LEFT];
-//       const isVisible = surroundingTrees.some((t) => t === null || tree > t);
-
-//       console.log(surroundingTrees, "of", tree, isVisible);
-
-//       isVisible && visibleTrees++;
-//     });
-//     console.log("...");
-//   });
-
-//   return visibleTrees;
-// }
-
-function visibleFromX(row, position, height) {
-  const tallestLeft = Math.max(...row.slice(null, position));
-  const tallestRight = Math.max(...row.slice(position + 1));
-
-  return height > tallestLeft || height > tallestRight;
-}
-
-function visibleFromY(row, position, height) {
-  const tallestLeft = Math.max(...row.slice(null, position));
-  const tallestRight = Math.max(...row.slice(position + 1));
-
-  return height > tallestLeft || height > tallestRight;
-}
-
 function getTreeColumn(x) {
   const column = [];
 
@@ -63,13 +27,20 @@ function getTreeColumn(x) {
   return column;
 }
 
+function isVisible(row, position, height) {
+  const tallestLeft = Math.max(...row.slice(null, position));
+  const tallestRight = Math.max(...row.slice(position + 1));
+
+  return height > tallestLeft || height > tallestRight;
+}
+
 function puzzleOne() {
   let visibleTrees = 0;
 
   treeMap.forEach((treeRow, y) => {
     treeRow.forEach((tree, x) => {
-      const isXVisible = visibleFromX(treeRow, x, tree);
-      const isYVisible = visibleFromY(getTreeColumn(x), y, tree);
+      const isXVisible = isVisible(treeRow, x, tree);
+      const isYVisible = isVisible(getTreeColumn(x), y, tree);
 
       (isXVisible || isYVisible) && visibleTrees++;
     });
@@ -78,5 +49,48 @@ function puzzleOne() {
   return visibleTrees;
 }
 
+function getDirectionalScenicScore(tree, direction) {
+  let score = 0;
+
+  for (let i = 0; i < direction.length; i++) {
+    const currentTree = direction[i];
+
+    if (tree <= currentTree) {
+      score++;
+      break;
+    } else {
+      score++;
+    }
+  }
+
+  return score;
+}
+
+function getScenicScore(row, column, x, y, tree) {
+  const up = getDirectionalScenicScore(tree, column.slice(null, y).reverse());
+  const right = getDirectionalScenicScore(tree, row.slice(x + 1));
+  const down = getDirectionalScenicScore(tree, column.slice(y + 1));
+  const left = getDirectionalScenicScore(tree, row.slice(null, x).reverse());
+
+  return up * right * down * left;
+}
+
+function puzzleTwo() {
+  let highestScenicScore = 0;
+
+  treeMap.forEach((treeRow, y) => {
+    treeRow.forEach((tree, x) => {
+      const scenicScore = getScenicScore(treeRow, getTreeColumn(x), x, y, tree);
+
+      if (highestScenicScore < scenicScore) {
+        highestScenicScore = scenicScore;
+      }
+    });
+  });
+
+  return highestScenicScore;
+}
+
 generateTreeMap();
-console.log(puzzleOne());
+console.log(`The number of visible trees is: ${puzzleOne()}`);
+console.log(`The highest scenic score is: ${puzzleTwo()}`);

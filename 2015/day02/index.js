a260643e33fde2ed773da2ecdebc2dd71d3bee86
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-21 00:06:36 +0100

    - finishing day 2

--- 2015/day02/index.js
+++ 2015/day02/index.js
@@ -21,4 +21,20 @@ function puzzleOne() {
   return total;
 }
 
+function puzzleTwo() {
+  let total = 0;
+
+  data.split("\n").forEach((line) => {
+    const [l, w, h] = line.split("x").map((x) => +x);
+    const [sm, md] = [l, w, h].sort((a, b) => (a > b ? 1 : b > a ? -1 : 0));
+    const ribbon = sm * 2 + md * 2;
+    const bow = l * w * h;
+
+    total += ribbon + bow;
+  });
+
+  return total;
+}
+
 console.log(`Square feet of wrapping paper required: ${puzzleOne()}`);
+console.log(`The length of ribbon required: ${puzzleTwo()}`);

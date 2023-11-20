d69ae81948cfe0bd73a0019ace461e997e33e39d
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-20 14:00:13 +0100

    - solves puzzle 1

--- 2022/day8/index.js
+++ 2022/day8/index.js
@@ -5,14 +5,78 @@ const example = `30373
 33549
 35390`;
 
-const source = example;
+const source = input;
 
 const lines = source.split("\n");
 const treeMap = [];
 
-lines.forEach((line) => {
-  const trees = line.split("").map((t) => +t);
-  treeMap.push(trees);
-});
+function generateTreeMap() {
+  lines.forEach((line) => {
+    const trees = line.split("").map((t) => +t);
+    treeMap.push(trees);
+  });
+}
 
-console.log(treeMap);
+// function puzzleOne() {
+//   let visibleTrees = 0;
+
+//   treeMap.forEach((treeRow, y) => {
+//     treeRow.forEach((tree, x) => {
+//       const ABOVE = treeMap[y - 1] != null ? treeMap[y - 1][x] : null;
+//       const RIGHT = treeRow[x + 1] != null ? treeRow[x + 1] : null;
+//       const BELOW = treeMap[y + 1] != null ? treeMap[y + 1][x] : null;
+//       const LEFT = treeRow[x - 1] != null ? treeRow[x - 1] : null;
+//       const surroundingTrees = [ABOVE, RIGHT, BELOW, LEFT];
+//       const isVisible = surroundingTrees.some((t) => t === null || tree > t);
+
+//       console.log(surroundingTrees, "of", tree, isVisible);
+
+//       isVisible && visibleTrees++;
+//     });
+//     console.log("...");
+//   });
+
+//   return visibleTrees;
+// }
+
+function visibleFromX(row, position, height) {
+  const tallestLeft = Math.max(...row.slice(null, position));
+  const tallestRight = Math.max(...row.slice(position + 1));
+
+  return height > tallestLeft || height > tallestRight;
+}
+
+function visibleFromY(row, position, height) {
+  const tallestLeft = Math.max(...row.slice(null, position));
+  const tallestRight = Math.max(...row.slice(position + 1));
+
+  return height > tallestLeft || height > tallestRight;
+}
+
+function getTreeColumn(x) {
+  const column = [];
+
+  treeMap.forEach((row) => {
+    column.push(row[x]);
+  });
+
+  return column;
+}
+
+function puzzleOne() {
+  let visibleTrees = 0;
+
+  treeMap.forEach((treeRow, y) => {
+    treeRow.forEach((tree, x) => {
+      const isXVisible = visibleFromX(treeRow, x, tree);
+      const isYVisible = visibleFromY(getTreeColumn(x), y, tree);
+
+      (isXVisible || isYVisible) && visibleTrees++;
+    });
+  });
+
+  return visibleTrees;
+}
+
+generateTreeMap();
+console.log(puzzleOne());

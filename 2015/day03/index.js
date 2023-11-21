bdf305aec1752b83a2e7f17775cdc0b982638117
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-21 01:17:33 +0100

    - fixes function

--- 2015/day03/index.js
+++ 2015/day03/index.js
@@ -1,24 +1,29 @@
 import input from "./input.js";
 
-const example = `>>^>vv<>>^^^<<<<<vv>>>>>`; // 19
+const example = `^^^^`; // 19
 
 const data = input;
 
 const map = [[true]];
 let x = 0;
 let y = 0;
-let length = map[0].length;
+let rx = 0;
+let ry = 0;
+let length = 1;
 
 function puzzleOne() {
-  data.split("").forEach((direction) => {
+  data.split("").forEach((direction, i) => {
+    // const isSanta = i % 2 === 0;
+
     switch (direction) {
       case "^":
-        if (y === 0) {
-          map.unshift(new Array(length).fill(false));
-        } else {
-          y--;
+        y--;
+        if (y === -1) {
+          map.unshift([...new Array(length)].fill(false));
+          y = 0;
         }
         map[y][x] = true;
+
         break;
       case ">":
         x++;
@@ -27,20 +32,23 @@ function puzzleOne() {
           length++;
         }
         map[y][x] = true;
+
         break;
       case "v":
         y++;
         if (y > map.length - 1) map.push(new Array(length).fill(false));
         map[y][x] = true;
+
         break;
       case "<":
-        if (x === 0) {
+        x--;
+        if (x === -1) {
           map.forEach((row) => row.unshift(false));
+          x = 0;
           length++;
-        } else {
-          x--;
         }
         map[y][x] = true;
+
         break;
     }
   });

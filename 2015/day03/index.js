0c10c32156f2a3cf1deb6d760f406f87670cb81f
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-21 01:34:20 +0100

    - makes puzzleOne prettier

--- 2015/day03/index.js
+++ 2015/day03/index.js
@@ -1,60 +1,20 @@
 import input from "./input.js";
 
-const example = `^^^^`; // 19
-
-const data = input;
-
-const map = [[true]];
-let x = 0;
-let y = 0;
-let rx = 0;
-let ry = 0;
-let length = 1;
-
 function puzzleOne() {
-  data.split("").forEach((direction, i) => {
-    // const isSanta = i % 2 === 0;
+  const visited = new Set();
+  const santaPos = [0, 0];
 
-    switch (direction) {
-      case "^":
-        y--;
-        if (y === -1) {
-          map.unshift([...new Array(length)].fill(false));
-          y = 0;
-        }
-        map[y][x] = true;
+  visited.add(santaPos.toString());
 
-        break;
-      case ">":
-        x++;
-        if (x > length - 1) {
-          map.forEach((row) => row.push(false));
-          length++;
-        }
-        map[y][x] = true;
+  for (const direction of input) {
+    if (direction === "^") santaPos[1]++;
+    if (direction === "v") santaPos[1]--;
+    if (direction === ">") santaPos[0]++;
+    if (direction === "<") santaPos[0]--;
+    visited.add(santaPos.toString());
+  }
 
-        break;
-      case "v":
-        y++;
-        if (y > map.length - 1) map.push(new Array(length).fill(false));
-        map[y][x] = true;
-
-        break;
-      case "<":
-        x--;
-        if (x === -1) {
-          map.forEach((row) => row.unshift(false));
-          x = 0;
-          length++;
-        }
-        map[y][x] = true;
-
-        break;
-    }
-  });
-
-  const uniqueDeliveries = map.flat().filter((delivered) => delivered);
-  return uniqueDeliveries.length;
+  return visited.size;
 }
 
 console.log(`Houses with at least one present: ${puzzleOne()}`);

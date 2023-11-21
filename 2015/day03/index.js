4ffcb2639aeece5b082f4082358caf72a590a275
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-21 01:44:12 +0100

    - finishing part 2

--- 2015/day03/index.js
+++ 2015/day03/index.js
@@ -6,15 +6,37 @@ function puzzleOne() {
 
   visited.add(santaPos.toString());
 
-  for (const direction of input) {
+  input.split("").forEach((direction) => {
     if (direction === "^") santaPos[1]++;
     if (direction === "v") santaPos[1]--;
     if (direction === ">") santaPos[0]++;
     if (direction === "<") santaPos[0]--;
     visited.add(santaPos.toString());
-  }
+  });
+
+  return visited.size;
+}
+
+function puzzleTwo() {
+  const visited = new Set();
+  const santaPos = [0, 0];
+  const roboSantaPos = [0, 0];
+
+  visited.add(santaPos.toString());
+
+  input.split("").forEach((direction, i) => {
+    const currentSanta = i % 2 === 0 ? santaPos : roboSantaPos;
+    if (direction === "^") currentSanta[1]++;
+    if (direction === "v") currentSanta[1]--;
+    if (direction === ">") currentSanta[0]++;
+    if (direction === "<") currentSanta[0]--;
+    visited.add(currentSanta.toString());
+  });
 
   return visited.size;
 }
 
 console.log(`Houses with at least one present: ${puzzleOne()}`);
+console.log(
+  `Houses with at least one present with RoboSanta 3000: ${puzzleTwo()}`
+);

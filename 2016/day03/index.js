db64e43e87e95c1cb295fa32215bacc8367f2361
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-22 10:01:15 +0100

    - finishing part 2

--- 2016/day03/index.js
+++ 2016/day03/index.js
@@ -20,4 +20,42 @@ function puzzleOne() {
   return validTriangles;
 }
 
+function puzzleTwo() {
+  let validTriangles = 0;
+  let triangleGroups = [];
+
+  let aLengths = [];
+  let bLengths = [];
+  let cLengths = [];
+
+  triangles.forEach((triangle) => {
+    const [a, b, c] = triangle
+      .split(/\s+/)
+      .filter(Boolean)
+      .map((numString) => +numString);
+
+    aLengths.push(a);
+    bLengths.push(b);
+    cLengths.push(c);
+
+    if (aLengths.length === 3) {
+      triangleGroups.push(aLengths, bLengths, cLengths);
+      aLengths = [];
+      bLengths = [];
+      cLengths = [];
+    }
+  });
+
+  triangleGroups.forEach((triangle) => {
+    const [a, b, c] = triangle;
+
+    const isValidTriangle = a + b > c && b + c > a && c + a > b;
+
+    isValidTriangle && validTriangles++;
+  });
+
+  return validTriangles;
+}
+
 console.log(`Number of valid triangles is: ${puzzleOne()}`);
+console.log(`Number of valid grouped triangles is: ${puzzleTwo()}`);

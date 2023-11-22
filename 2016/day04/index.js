30da66846cb464f5b81a7809c6d099dfc26793d3
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-22 13:53:42 +0100

    - finishing part 1

--- 2016/day04/index.js
+++ 2016/day04/index.js
@@ -5,10 +5,47 @@ a-b-c-d-e-f-g-h-987[abcde]
 not-a-real-room-404[oarel]
 totally-real-room-200[decoy]`;
 
-const data = example.split("\n");
+const data = input.split("\n");
+
+function isRealRoom(encrypt, checksum) {
+  const charCount = {};
+
+  // Check occurances
+  encrypt.split("").forEach((char) => {
+    charCount[char] = charCount[char] ? charCount[char] + 1 : 1;
+  });
+
+  // Sort by occurance
+  const sortedCharCount = Object.entries(charCount).sort((a, b) => {
+    if (a[1] === b[1]) {
+      // If occurance is the same, sort alphabetically
+      return a[0] > b[0] ? 1 : -1;
+    } else {
+      // Sort by occurance
+      return b[1] - a[1];
+    }
+  });
+
+  const encryptChecksum = sortedCharCount
+    .slice(0, 5)
+    .map((a) => a[0])
+    .join("");
+
+  return encryptChecksum === checksum;
+}
 
 function puzzleOne() {
   let sectorIdSum = 0;
+
+  data.map((room) => {
+    const encrypts = room.split("-");
+    const sectorIdAndChecksum = encrypts.pop();
+    const sectorId = +sectorIdAndChecksum.split("[")[0].split("]")[0];
+    const checksum = sectorIdAndChecksum.split("[")[1].split("]")[0];
+    const fullEncrypt = encrypts.join("");
+
+    if (isRealRoom(fullEncrypt, checksum)) sectorIdSum += sectorId;
+  });
 
   return sectorIdSum;
 }

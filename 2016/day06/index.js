ff8eff547f2155d7e691a494142c82ac060da125
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-24 09:30:35 +0100

    - finishing day 6

--- 2016/day06/index.js
+++ 2016/day06/index.js
@@ -18,7 +18,7 @@ vrdear
 dvrsen
 enarar`;
 
-const data = example.split("\n");
+const data = input.split("\n");
 
 function puzzleOne() {
   const columns = new Array(data[0].length).fill(null).map(() => []);
@@ -30,10 +30,45 @@ function puzzleOne() {
   });
 
   columns.forEach((col) => {
-    // sort col by occurrance and += message with first char
+    const chars = {};
+    col.forEach((char) => (chars[char] = chars[char] ? chars[char] + 1 : 1));
+
+    const sortedChars = Object.entries(chars).sort((a, b) =>
+      a[1] > b[1] ? -1 : b[1] > a[1] ? 1 : 0
+    );
+
+    sortedChars.forEach((arr, i) => {
+      if (i === 0) message += arr[0];
+    });
+  });
+
+  return message;
+}
+
+function puzzleTwo() {
+  const columns = new Array(data[0].length).fill(null).map(() => []);
+  let message = "";
+
+  data.forEach((line) => {
+    const chars = line.split("");
+    chars.forEach((char, i) => columns[i].push(char));
+  });
+
+  columns.forEach((col) => {
+    const chars = {};
+    col.forEach((char) => (chars[char] = chars[char] ? chars[char] + 1 : 1));
+
+    const sortedChars = Object.entries(chars).sort((a, b) =>
+      a[1] > b[1] ? 1 : b[1] > a[1] ? -1 : 0
+    );
+
+    sortedChars.forEach((arr, i) => {
+      if (i === 0) message += arr[0];
+    });
   });
 
   return message;
 }
 
 console.log(`The error-corrected version of the message is ${puzzleOne()}`);
+console.log(`The actual version of the message is ${puzzleTwo()}`);

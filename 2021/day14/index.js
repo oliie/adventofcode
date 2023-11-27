039a4511b08c233c8c69f482ee29d4b28598a275
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-27 12:42:22 +0100

    - wip

--- 2021/day14/index.js
+++ 2021/day14/index.js
@@ -20,9 +20,43 @@ CC -> N
 CN -> C`;
 
 const data = example;
+let template = data.split("\n")[0];
+
+let templates = [];
+const pairs = {};
+
+data
+  .split("\n\n")[1]
+  .split("\n")
+  .forEach((rule) => {
+    const [input, output] = rule.split(" -> ");
+    pairs[input] = output;
+  });
+
+for (let i = 0; i < template.length - 1; i++) {
+  templates.push(template.slice(i, i + 2));
+}
 
 function puzzle() {
   const sum = 0;
+  const pairCount = {};
+
+  for (let i = 0; i < 10; i++) {
+    const nextTemplates = [];
+
+    for (const t of templates) {
+      pairCount[t[0]] ? pairCount[t[0]]++ : (pairCount[t[0]] = 1);
+      pairCount[t[1]] ? pairCount[t[1]]++ : (pairCount[t[1]] = 1);
+      const nextPairsString = t.split("").join(pairs[t]);
+      const first = nextPairsString[0] + nextPairsString[1];
+      const second = nextPairsString[1] + nextPairsString[2];
+      nextTemplates.push(first, second);
+    }
+
+    templates = nextTemplates;
+  }
+
+  console.log(pairCount);
 
   return sum;
 }

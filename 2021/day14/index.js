752ed46504d7860d9ba8c90cf90c5aae5e0f7433
Oliver Praesto <oliver.praesto@regionstockholm.se>
2023-11-27 09:44:16 +0100

    - starts day 14

---
+++ 2021/day14/index.js
@@ -0,0 +1,32 @@
+import input from "./input.js";
+
+const example = `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`;
+
+const data = example;
+
+function puzzle() {
+  const sum = 0;
+
+  return sum;
+}
+
+console.log(
+  `The most common element minus the least common element is ${puzzle()} after 10 iterations.`
+);
